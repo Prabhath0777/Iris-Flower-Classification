1d72c0c2457d21e6b8709ac04e33a6704111a5de
Prabhath0777 <152522775+Prabhath0777@users.noreply.github.com>
2026-08-09 22:53:45 +0530

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,1 +1,27 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
 
+
+def test_iris_model():
+    iris = load_iris()
+
+    X = iris.data
+    y = iris.target
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    scaler = StandardScaler()
+
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    model = LogisticRegression(max_iter=200)
+    model.fit(X_train, y_train)
+
+    accuracy = model.score(X_test, y_test)
+
+    assert accuracy > 0.90
